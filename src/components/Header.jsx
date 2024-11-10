36d52735e378198977419c6dca566eff438e3ff4
Nieem <nieemcse019@gmail.com>
2024-11-11 00:32:45 +0600

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Logo from '../assets/logo.svg'
 
 const Header = () => {
     return (
-        <div className='bg-red-400'>
-            header
-        </div>
+        <header className='py-8'>
+            <div className='container mx-auto'>
+                <div className='flex justify-between items-center'>
+                    <a>
+                    <img src={Logo} alt=''/>
+                    </a>
+                    <button className='btn btn-sm'>Work with me</button>
+
+                </div>
+            </div>
+        </header>
     );
 };
 
